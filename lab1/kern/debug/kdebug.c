5535700bb0f4b4d9e2616d9ae7d639c7a5a1c12f
“Facwl” <2864235156@qq.com>
2020-10-09 18:58:58 +0800

    exercise5 by D

--- lab1/kern/debug/kdebug.c
+++ lab1/kern/debug/kdebug.c
@@ -302,5 +302,27 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+	//（1）(2）
+	uint32_t ebp = read_ebp();
+	uint32_t eip = read_eip();	//找到 base pointer
+	//(3)
+	int i,j;//why????
+	for(i=0;i<STACKFRAME_DEPTH&&ebp!=0;i++)//没到底
+	{
+		cprintf("ebp:0x%08x eip:0x%08x args:", ebp, eip); 
+		uint32_t *args = (uint32_t *)ebp + 2; //参数的首地址 == (uint32_t *)(ebp+8)
+		for (j = 0; j < 4; j++) 
+		{
+		    cprintf("0x%08x ", args[j]); //打印4个参数
+		}
+		cprintf("\n");
+		print_debuginfo(eip - 1);  //打印函数信息(???原理)
+		eip = ((uint32_t *)ebp)[1]; //强行4字节，更新eip
+		ebp = ((uint32_t *)ebp)[0]; //更新ebp
+	}
+	
+	
+	
+	
 }
 
