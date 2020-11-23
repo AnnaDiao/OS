8a6da4e11757ae76f8638339a62b7aaa5d93538b
VitalC-3026 <1184649102@qq.com>
2020-11-23 04:49:50 -0800

    lab3 exe1&2

---
+++ lab3/kern/mm/swap_clock.c
@@ -0,0 +1,145 @@
+#include <defs.h>
+#include <x86.h>
+#include <stdio.h>
+#include <string.h>
+#include <swap.h>
+#include <swap_clock.h>
+#include <list.h>
+
+list_entry_t pra_list_head;
+
+static int
+_clock_init_mm(struct mm_struct *mm)
+{     
+     list_init(&pra_list_head);
+     mm->sm_priv = &pra_list_head;
+     //cprintf(" mm->sm_priv %x in fifo_init_mm\n",mm->sm_priv);
+     return 0;
+}
+/*
+ * (3)_clock_map_swappable: According FIFO PRA, we should link the most recent arrival page at the back of pra_list_head qeueue
+ */
+static int
+_clock_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int swap_in)
+{
+    list_entry_t *head=(list_entry_t*) mm->sm_priv;
+    list_entry_t *entry=&(page->pra_page_link);
+ 
+    assert(entry != NULL && head != NULL);
+    // struct Page* p = le2page(entry, pra_page_link);
+    // pte_t *pte;
+    // if (pte = get_pte(mm->pgdir, p->pra_vaddr, 0) == NULL) {
+    //     cprintf("clock_map_swappable failed.\n");
+    // }
+    // *pte &= ~PTE_A; 
+    list_add_before(head, entry);
+    
+    return 0;
+}
+/*
+ *  (4)_clock_swap_out_victim: According FIFO PRA, we should unlink the  earliest arrival page in front of pra_list_head qeueue,
+ *                            then set the addr of addr of this page to ptr_page.
+ */
+static int
+_clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
+{
+    list_entry_t *head=(list_entry_t*) mm->sm_priv;
+        assert(head != NULL);
+    assert(in_tick==0);
+    /* Select the victim */
+    /*LAB3 EXERCISE 2: YOUR CODE*/
+    //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue 
+    //(2)  set the addr of addr of this page to ptr_page
+    list_entry_t *le = list_next(head);
+    while  ((le = list_next(le)) != head) {    
+        struct Page *p = le2page(le, pra_page_link);
+        pte_t *pte = get_pte(mm->pgdir, p->pra_vaddr, 0);
+        if (*pte & PTE_A) {
+            *pte &= ~PTE_A;
+        } else {
+            *ptr_page = p;
+            list_del(p);
+            break;
+        }
+                           
+    //     struct Page *p = le2page(le, pra_page_link);
+    //     assert(p != NULL);
+    //     *ptr_page = p;
+    // } else {
+    //     *ptr_page = NULL;
+    }
+    list_del(le);   
+    
+    return 0;
+}
+
+static int
+_clock_check_swap(void) {
+    cprintf("write Virt Page c in clock_check_swap\n");
+    *(unsigned char *)0x3000 = 0x0c;
+    assert(pgfault_num==4);
+    cprintf("write Virt Page a in clock_check_swap\n");
+    *(unsigned char *)0x1000 = 0x0a;
+    assert(pgfault_num==4);
+    cprintf("write Virt Page d in clock_check_swap\n");
+    *(unsigned char *)0x4000 = 0x0d;
+    assert(pgfault_num==4);
+    cprintf("write Virt Page b in clock_check_swap\n");
+    *(unsigned char *)0x2000 = 0x0b;
+    assert(pgfault_num==4);
+    cprintf("write Virt Page e in clock_check_swap\n");
+    *(unsigned char *)0x5000 = 0x0e;
+    assert(pgfault_num==5);
+    cprintf("write Virt Page b in clock_check_swap\n");
+    *(unsigned char *)0x2000 = 0x0b;
+    assert(pgfault_num==5);
+    cprintf("write Virt Page a in clock_check_swap\n");
+    *(unsigned char *)0x1000 = 0x0a;
+    assert(pgfault_num==6);
+    cprintf("write Virt Page b in clock_check_swap\n");
+    *(unsigned char *)0x2000 = 0x0b;
+    assert(pgfault_num==7);
+    cprintf("write Virt Page c in clock_check_swap\n");
+    *(unsigned char *)0x3000 = 0x0c;
+    assert(pgfault_num==8);
+    cprintf("write Virt Page d in clock_check_swap\n");
+    *(unsigned char *)0x4000 = 0x0d;
+    assert(pgfault_num==9);
+    cprintf("write Virt Page e in clock_check_swap\n");
+    *(unsigned char *)0x5000 = 0x0e;
+    assert(pgfault_num==10);
+    cprintf("write Virt Page a in clock_check_swap\n");
+    *(unsigned char *)0x1000 = 0x0a;
+    assert(pgfault_num==11);
+    return 0;
+}
+
+
+static int
+_clock_init(void)
+{
+    return 0;
+}
+
+static int
+_clock_set_unswappable(struct mm_struct *mm, uintptr_t addr)
+{
+    return 0;
+}
+
+static int
+_clock_tick_event(struct mm_struct *mm)
+{ return 0; }
+
+
+struct swap_manager swap_manager_clock =
+{
+     .name            = "clock swap manager",
+     .init            = &_clock_init,
+     .init_mm         = &_clock_init_mm,
+     .tick_event      = &_clock_tick_event,
+     .map_swappable   = &_clock_map_swappable,
+     .set_unswappable = &_clock_set_unswappable,
+     .swap_out_victim = &_clock_swap_out_victim,
+     .check_swap      = &_clock_check_swap,
+};
